dc05037da5dff34d4a0bb5c5a0e6ae14764dccc5
Callum Macfarlane <CallumDMacfarlane@live.com>
2023-02-01 18:46:07 +1000

    Ymal loader fix

--- config.py
+++ config.py
@@ -14,12 +14,13 @@ if not exists(YAML_CONFIG_FILE):
     raise Exception("config.yml file not found, modify config.yml.example file change it's name to config.yml")
 
 with open(YAML_CONFIG_FILE) as fp:
-    yaml_config = yaml.load(fp)
+    yaml_config = yaml.load(fp, Loader=yaml.FullLoader)
 
-NAUTY_EXECUTABLE = yaml_config["NAUTY_EXECUTABLE"] \
-    if yaml_config["NAUTY_EXECUTABLE"].startswith("/") \
-    else join(THIS_DIR, yaml_config["NAUTY_EXECUTABLE"])
+# NAUTY_EXECUTABLE = yaml_config["NAUTY_EXECUTABLE"] \
+#     if yaml_config["NAUTY_EXECUTABLE"].startswith("/") \
+#     else join(THIS_DIR, yaml_config["NAUTY_EXECUTABLE"])
+NAUTY_EXECUTABLE = yaml_config["NAUTY_EXECUTABLE"]
 
-assert NAUTY_EXECUTABLE[0] == '/', 'Dreadnaut executable was not an absolute path: "{0}"'.format(NAUTY_EXECUTABLE)
+# assert NAUTY_EXECUTABLE[0] == '/', 'Dreadnaut executable was not an absolute path: "{0}"'.format(NAUTY_EXECUTABLE)
 
 assert exists(NAUTY_EXECUTABLE), 'Could not find dreadnaut executable at: "{0}". Did you install nauty (http://users.cecs.anu.edu.au/~bdm/nauty/) ?'.format(NAUTY_EXECUTABLE)
